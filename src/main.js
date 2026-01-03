a94c5c21a0995af36f158feae6c562ad64540205
Aleksandr Chertkov <alexiiilaiho@gmail.com>
2026-01-03 16:02:55 +0300

    Stage 4 completed

--- src/main.js
+++ src/main.js
@@ -40,10 +40,9 @@ async function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let query = {};
     // @todo: использование
-    // result = applySearching(result, state, action);
+    query = applySearching(query, state, action); // result заменяем на query
     query = applyFiltering(query, state, action); // result заменяем на query
     // result = applySorting(result, state, action)
-    // result = applyPagination(result, state, action); //
      query = applyPagination(query, state, action); // обновляем query
 
     const { total, items } = await api.getRecords(query); // запрашиваем данные с собранными параметрами
